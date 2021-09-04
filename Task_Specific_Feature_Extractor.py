fcfea2884d2bebf79b15e132e379a54d74bf926d
Kwanit Gupta <gupta.45@iitj.ac.in>
2021-09-05 02:08:03 +0530

    Update Task_Specific_Feature_Extractor.py

--- Task_Specific_Feature_Extractor.py
+++ Task_Specific_Feature_Extractor.py
@@ -4,10 +4,8 @@ import pandas as pd
 import numpy as np
 import os
 
-def x_ray_task_specific(Model_base_directory,train_embed_directory,test_embed_directory,train_labels,test_labels):
+def x_ray_task_specific(Model_save_directory,train_embed,test_embed,train_labels,test_labels):
 
-	x_train = np.load(train_embed_directory)
-	x_test = np.load(test_embed_directory)
 	x_task = keras.Sequential()
 	x_task.add(keras.layers.BatchNormalization())
 	x_task.add(keras.layers.Dense(1120))
@@ -20,14 +18,12 @@ def x_ray_task_specific(Model_base_directory,train_embed_directory,test_embed_di
 	x_task.add(keras.layers.LeakyReLU())
 	x_task.add(keras.layers.BatchNormalization())
 	x_task.add(keras.layers.Dense(1))
-	save_callback = keras.calbacks.ModelCheckpoint(Model_base_directory,monitor='val_accuracy',verbose=1,save_best_only=True,mode='max')
+	save_callback = keras.callbacks.ModelCheckpoint(Model_save_directory,monitor='val_accuracy',verbose=1,save_best_only=True,mode='max')
 	x_task.compile(optimizer=keras.optimizers.Adam(),loss=keras.losses.BinaryCrossentropy(from_logits=True),metrics=['accuracy',keras.metrics.AUC(from_logits=True)])
-	hist = x_task.fit(x=x_train,y=train_labels,epochs=100,verbose=1,validation_data=(x_test,test_labels),use_multiprocessing=True,callbacks=save_callback)
+	hist = x_task.fit(x=train_embed,y=train_labels,epochs=100,verbose=1,validation_data=(test_embed,test_labels),use_multiprocessing=True,callbacks=save_callback)
 
-def ct_scan_task_specific(Model_base_directory,train_embed_directory,test_embed_directory,train_labels,test_labels):
+def ct_scan_task_specific(Model_save_directory,train_embed,test_embed,train_labels,test_labels):
 
-	ct_train = np.load(train_embed_directory)
-	ct_test = np.load(test_embed_directory)
 	ct_task = keras.Sequential()
 	ct_task.add(keras.layers.BatchNormalization())
 	ct_task.add(keras.layers.Dense(1120))
@@ -43,7 +39,6 @@ def ct_scan_task_specific(Model_base_directory,train_embed_directory,test_embed_
 	ct_task.add(keras.layers.LeakyReLU(0.5))
 	ct_task.add(keras.layers.BatchNormalization())
 	ct_task.add(keras.layers.Dense(1))
-	save_callback = keras.calbacks.ModelCheckpoint(Model_base_directory,monitor='val_accuracy',verbose=1,save_best_only=True,mode='max')
+	save_callback = keras.callbacks.ModelCheckpoint(Model_save_directory,monitor='val_accuracy',verbose=1,save_best_only=True,mode='max')
 	ct_task.compile(optimizer=keras.optimizers.Adam(),loss=keras.losses.BinaryCrossentropy(from_logits=True),metrics=['accuracy',keras.metrics.AUC(from_logits=True)])
-	hist = ct_task.fit(x=ct_train,y=train_labels,epochs=100,verbose=1,validation_data=(ct_test,test_labels),use_multiprocessing=True,callbacks=save_callback)
-		
+	hist = ct_task.fit(x=train_embed,y=train_labels,epochs=100,verbose=1,validation_data=(test_embed,test_labels),use_multiprocessing=True,callbacks=save_callback)
