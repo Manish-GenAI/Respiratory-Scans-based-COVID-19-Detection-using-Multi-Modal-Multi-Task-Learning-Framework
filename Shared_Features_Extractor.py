aca630114d1aaed2d532270f0451eee4eedc1708
Kwanit Gupta <gupta.45@iitj.ac.in>
2021-09-05 12:23:32 +0530

    Update Shared_Features_Extractor.py

--- Shared_Features_Extractor.py
+++ Shared_Features_Extractor.py
@@ -16,7 +16,6 @@ def disc_model():
 	disc.add(keras.layers.Dense(1))
 	return disc
 
-
 def disc_loss_func(actual_embed,gen_embed,train_label,invert_label):
 	actual_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=train_label, logits=actual_embed)
 	feature_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=invert_label, logits=gen_embed)
@@ -36,43 +35,39 @@ def gen_model():
 	gen.add(keras.layers.LeakyReLU())
 	return gen
 
+
 def gen_loss_func(gen_embed,invert_label):
 	return tf.nn.sigmoid_cross_entropy_with_logits(labels = invert_label, logits = gen_embed)
 
-def train_step(reference_embed,old_gen_loss,old_disc_loss,train_label,invert_label):
+def train_step(actual_embed,old_gen_loss,old_disc_loss,train_label,invert_label):
 	with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-		features = gen(reference_embed, training=True)
-		real_output = disc(reference_embed, training=True)
-		features_output = disc(reference_embed, training=True)
+		features = gen(actual_embed, training=True)
+		real_output = disc(actual_embed, training=True)
+		features_output = disc(features, training=True)
 		gen_loss = gen_loss_func(features_output,invert_label)
 		disc_loss = disc_loss_func(real_output,features_output,train_label,invert_label)
-
 	gradients_of_generator = gen_tape.gradient(gen_loss, gen.variables)
 	gen_optim.apply_gradients(zip(gradients_of_generator, gen.trainable_variables))
 	gradients_of_discriminator = disc_tape.gradient(disc_loss, disc.variables)
 	disc_optim.apply_gradients(zip(gradients_of_discriminator, disc.trainable_variables))
 	return gen_loss,disc_loss
 
-def fine_tune(Model_base_Directory,train_label,test_label,x_train_embed,x_test_embed,ct_train_embed,ct_test_embed):
+def fine_tune(Model_save_Directory,model_gen,x_train_embed,x_test_embed,ct_train_embed,ct_test_embed):
 	x_train_label = np.ones((x_train_embed.shape[0],))
 	ct_train_label = np.zeros((ct_train_embed.shape[0],))
 	x_test_label = np.ones((x_test_embed.shape[0],))
 	ct_test_label = np.zeros((ct_test_embed.shape[0],))
-	train_label = np.concatenate((x_train_label,ct_train_label),axis==0)
-	test_label = np.concatenate((x_test_label,ct_test_label),axis==0)
-	train_embed = np.concatenate((x_train_embed,ct_train_embed),axis==0)
-	test_embed = np.concatenate((x_test_embed,ct_test_embed),axis==0)
-	gen = keras.models.load_model(Model_base_Directory)
-	gen.trainable=False
+	train_label = np.concatenate((x_train_label,ct_train_label),axis=0)
+	test_label = np.concatenate((x_test_label,ct_test_label),axis=0)
+	train_embed = np.concatenate((x_train_embed,ct_train_embed),axis=0)
+	test_embed = np.concatenate((x_test_embed,ct_test_embed),axis=0)
+	model_gen.trainable=False
 	model = keras.Sequential()
-	model.add(gen)
+	model.add(model_gen)
 	model.add(keras.layers.LeakyReLU())
 	model.add(keras.layers.Dense(640))
 	model.add(keras.layers.LeakyReLU())
 	model.add(keras.layers.Dense(1,activation='sigmoid'))
 	model.compile(optimizer='Adam',loss=keras.losses.BinaryCrossentropy(from_logits=False),metrics=['accuracy',keras.metrics.AUC(from_logits=False)])
-	save = keras.callbacks.ModelCheckpoint(Model_base_Directory,monitor='val_accuracy',mode='max',save_best_only=True,verbose=1)
+	save = keras.callbacks.ModelCheckpoint(Model_save_Directory,monitor='val_accuracy',mode='max',save_best_only=True,verbose=1)
 	hist = model.fit(x=train_embed,y=train_label,validation_data=(test_embed,test_label),epochs=100,verbose=1,use_multiprocessing=True,callbacks=save)
-
-disc_optim = tf.optimizers.Adam(0.00001)
-gen_optim = tf.optimizers.Adam(0.00001)
