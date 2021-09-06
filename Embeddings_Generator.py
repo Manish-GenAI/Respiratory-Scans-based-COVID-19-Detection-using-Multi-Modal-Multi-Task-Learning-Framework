3012520601243fe3c1960fa485b6360ab86c1c01
Kwanit Gupta <gupta.45@iitj.ac.in>
2021-09-07 00:23:38 +0530

    Update Embeddings_Generator.py

--- Embeddings_Generator.py
+++ Embeddings_Generator.py
@@ -5,7 +5,6 @@ import tensorflow as tf
 from tensorflow import keras
 
 def Embedding_Model(Model_save_directory,Model_Class,Dataset_train_Directory,Dataset_test_Directory):
-	
 	if (Model_Class != keras.applications.NASNetLarge):
 		ds_train = keras.preprocessing.image_dataset_from_directory(Dataset_train_Directory,
 						labels = 'inferred',
@@ -29,7 +28,6 @@ def Embedding_Model(Model_save_directory,Model_Class,Dataset_train_Directory,Dat
 		save_callback = keras.callbacks.ModelCheckpoint(Model_save_directory,monitor='val_accuracy',verbose=1,save_best_only=True,mode='max')
 		model.compile(optimizer=keras.optimizers.Adam(),loss=keras.losses.BinaryCrossentropy(from_logits=True),metrics=['accuracy',keras.metrics.AUC(from_logits=True)])
 		hist = model.fit(ds_train,epochs=100,verbose=1,validation_data=ds_validation,use_multiprocessing=True,callbacks=save_callback)
-
 	else:
 		ds_train = keras.preprocessing.image_dataset_from_directory(Dataset_train_Directory,
 						labels = 'inferred',
@@ -56,15 +54,15 @@ def Embedding_Model(Model_save_directory,Model_Class,Dataset_train_Directory,Dat
 
 def Embedding_Save(Embed_save_Directory,Trained_Model_Class,Dataset_Directory,Image_Size):
 	if (Image_Size != 331):
-		x_train = keras.preprocessing.image_dataset_from_directory(Dataset_Directory,label_mode=None,labels=None,batch_size=32,image_size=(Image_Size,Image_Size)))
+		x = keras.preprocessing.image_dataset_from_directory(Dataset_Directory,label_mode=None,labels=None,batch_size=32,image_size=(Image_Size,Image_Size))
 	else:
-		x_train = keras.preprocessing.image_dataset_from_directory(Dataset_Directory,label_mode=None,labels=None,batch_size=32,image_size=(331,331)))
+		x = keras.preprocessing.image_dataset_from_directory(Dataset_Directory,label_mode=None,labels=None,batch_size=32,image_size=(331,331))
 	Trained_Model_Class.trainable=False
 	Trained_Model_Class.pop()
 	np_embed = []
-	for x in x_train:
-		interm = Trained_Model_Class(x).numpy().tolist()
-		for inte in interm:
-			np_embed.append(inte)
+	for xi in x:
+		interm = Trained_Model_Class(xi).numpy().tolist()
+		for i in interm:
+			np_embed.append(i)
 	np_embed = np.array(np_embed)
 	np.save(Embed_save_Directory,np_embed)
